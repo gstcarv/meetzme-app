f2bb16a51f39fe7cff0c8f3d127ae55022f40834
Gustavo Carvalho <gstcarv@gmail.com>
2019-01-24 21:05:21 -0200

    Navigation Routes

--- android/app/src/main/java/com/meetzme/MainApplication.java
+++ android/app/src/main/java/com/meetzme/MainApplication.java
@@ -3,6 +3,7 @@ package com.meetzme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage(),
             new SvgPackage()
---
+++ src/screens/Principal/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Principal extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Principal Screen</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
